1abbdd90364f4fa253802682878fe6ab87416bdf
patriceNEUSSIofficiel <patriceneussi9@gmail.com>
2024-01-31 15:47:20 +0100

    V5

---
+++ reservation/api/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from api.models import Category, Location, Bus, Schedule, Booking, Email
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Location)
+admin.site.register(Bus)
+admin.site.register(Schedule)
+admin.site.register(Booking)
+admin.site.register(Email)
+
